66393f0131235afe5833793a7e3b688861512b7b
JainikVK18 <Jainikpatel2007@gmail.com>
2024-12-24 14:31:20 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 const m3u8Links = {
- 1: "https://example.com/path-to-m3u81.m3u8",
+ 1: "https://vkvsd63.okcdn.ru/cmaf/7479532849895/sig/594OQraOF7w/expires/1735128760318/srcIp/13.233.212.210/urls/185.226.52.66/clientType/13/srcAg/CHROME/mid/9171296398311/get/hls_9171296398311.cGxuCiq8K1E.m3u8"
  2: "https://example.com/path-to-m3u82.m3u8",
 };
