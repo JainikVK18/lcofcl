33cab5b7d5347e59a0cbf0d37f0c2af2715304fd
JainikVK18 <Jainikpatel2007@gmail.com>
2024-12-24 14:53:54 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,13 @@
 const m3u8Links = {
- 1: "https://vkvsd63.okcdn.ru/cmaf/7479532849895/sig/594OQraOF7w/expires/1735128760318/srcIp/13.233.212.210/urls/185.226.52.66/clientType/13/srcAg/CHROME/mid/9171296398311/get/hls_9171296398311.cGxuCiq8K1E.m3u8"
+ 1: "https://example.com/path-to-m3u81.m3u8",
  2: "https://example.com/path-to-m3u82.m3u8",
+3: "https://example.com/path-to-m3u81.m3u8",
+ 4: "https://prod-sports-eng-gm.jiocinema.com/hls/live/2100307/hd_akamai_merged_avc_eng_cricket_m5_241224/master.m3u8",
+5: "https://example.com/path-to-m3u81.m3u8",
+ 6: "https://example.com/path-to-m3u82.m3u8",
+7: "https://prod-sports-eng-gm.jiocinema.com/hls/live/2100307/hd_akamai_merged_avc_eng_cricket_m10_241224/master.m3u8",
+ 8: "https://example.com/path-to-m3u82.m3u8",
+9: "https://example.com/path-to-m3u81.m3u8",
+ 10: "https://vkvsd63.okcdn.ru/cmaf/7479532849895/sig/594OQraOF7w/expires/1735128760318/srcIp/13.233.212.210/urls/185.226.52.66/clientType/13/srcAg/CHROME/mid/9171296398311/get/hls_9171296398311.cGxuCiq8K1E.m3u8",
+
 };
