03f20ec5994a4991b77540d9a7729cb83510e6a0
JainikVK18 <Jainikpatel2007@gmail.com>
2024-12-27 19:24:32 -0500

    Update data.js

--- data.js
+++ data.js
@@ -13,4 +13,5 @@ const m3u8Links = {
   12:"https://vkvsd63.okcdn.ru/cmaf/7465941011175/sig/WoppvWS6x7o/srcIp/52.66.211.175/urls/185.226.53.94/expires/1735590625008/clientType/13/srcAg/CHROME/fromCache/1/mid/9161388141543/id/7465941011175/get/hls_9161388141543.yi5Gc1pbmz0.m3u8",
   13:"https://vkvsd55.okcdn.ru/cmaf/7517976988391/sig/R2o03V3qTPI/expires/1735351147101/srcIp/13.233.232.111/urls/45.136.22.21/clientType/13/srcAg/CHROME/mid/9200744410087/get/hls_9200744410087.GYAkhiUddgo.m3u8",
   14:"https://jcevents.jiocinema.com/bpk-tv/Sports18_Khel_voot_MOB/Fallback/hdntl=exp=1735392758~acl=/*~id=aaf549ec51984c86b32dd7de3e1334c7~data=hdntl~hmac=626a9fc78477bd893c235104da6ecd019ee660e673581b5252e6e194525aea81/Sports18_Khel_voot_MOB-audio_98838_eng=98800-video=1680400/index.m3u8",
+  15:"https://vkvsd241.okcdn.ru/hls/7590916983401.m3u8/sig/pX5s-y7MN9Q/srcIp/92.62.121.84/urls/185.226.52.55/expires/1735795235815/clientType/14/srcAg/CHROME/fromCache/1/mid/8838824930153/id/7590916983401/7590916983401_high/index.m3u8",
 };
