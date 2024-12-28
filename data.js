a7fee28f4213713fc3f840fedc5848b8dce17bf3
JainikVK18 <Jainikpatel2007@gmail.com>
2024-12-28 14:04:32 -0500

    Update data.js

--- data.js
+++ data.js
@@ -11,4 +11,5 @@ const m3u8Links = {
   10: "https://vkvsd63.okcdn.ru/cmaf/7479532849895/sig/CxLFpNZXBhY/srcIp/3.108.6.73/urls/185.226.52.66/expires/1735855595623/clientType/13/srcAg/CHROME/fromCache/1/mid/9171296398311/id/7479532849895/get/hls_9171296398311.cGxuCiq8K1E.m3u8",
   11:"https://vkvsd55.okcdn.ru/cmaf/7517976988391/sig/qhLpbAnnU5o/srcIp/13.203.71.74/urls/45.136.22.21/expires/1735811040971/clientType/13/srcAg/CHROME/fromCache/1/mid/9200744410087/id/7517976988391/get/hls_9200744410087.GYAkhiUddgo.m3u8",
   12:"https://vkvsd55.okcdn.ru/cmaf/7465941011175/sig/uvJeXZhUciA/srcIp/13.201.182.224/urls/45.136.21.63/expires/1735856029357/clientType/13/srcAg/CHROME/fromCache/1/mid/9161388141543/id/7465941011175/get/hls_9161388141543.yi5Gc1pbmz0.m3u8",
+  13:"https://m118753.protect-cdn.net/live/m1-index.m3u8",
 };
