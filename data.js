6841b174521a23bad1876ac35edd146cd5aea80d
JainikVK18 <Jainikpatel2007@gmail.com>
2025-01-02 16:21:38 -0500

    Update data.js

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ const m3u8Links = {
   7: "https://prod-sports-eng-gm.jiocinema.com/hls/live/2100307/hd_akamai_merged_avc_eng_cricket_m10_241224/master.m3u8",
   8: "https://example.com/path-to-m3u82.m3u8",
   9: "https://example.com/path-to-m3u81.m3u8",
-  10: "https://vkvsd63.okcdn.ru/cmaf/7479532849895/sig/CxLFpNZXBhY/srcIp/3.108.6.73/urls/185.226.52.66/expires/1735855595623/clientType/13/srcAg/CHROME/fromCache/1/mid/9171296398311/id/7479532849895/get/hls_9171296398311.cGxuCiq8K1E.m3u8",
+  10: "https://vkvsd14.okcdn.ru/cmaf/7570831313639/sig/2jttp-qrPhw/srcIp/65.2.31.89/urls/45.136.20.60/expires/1736311968382/clientType/13/srcAg/CHROME/fromCache/1/mid/9239821364199/id/7570831313639/get/hls_9239821364199.vZSE3S7XUTE.m3u8",
   11:"https://vkvsd55.okcdn.ru/cmaf/7517976988391/sig/S6ydEqSk3vk/expires/1735912357699/srcIp/13.203.23.39/urls/45.136.22.21/clientType/13/srcAg/CHROME/mid/9200744410087/get/hls_9200744410087.GYAkhiUddgo.m3u8",
   12:"https://vkvsd55.okcdn.ru/cmaf/7465941011175/sig/uvJeXZhUciA/srcIp/13.201.182.224/urls/45.136.21.63/expires/1735856029357/clientType/13/srcAg/CHROME/fromCache/1/mid/9161388141543/id/7465941011175/get/hls_9161388141543.yi5Gc1pbmz0.m3u8",
   13:"https://m118753.protect-cdn.net/live/m1-index.m3u8",
