de1a3d2289c00c67d89e42acdea895b216ee81d6
JainikVK18 <Jainikpatel2007@gmail.com>
2025-01-03 12:43:50 -0500

    Update data.js

--- data.js
+++ data.js
@@ -9,6 +9,6 @@ const m3u8Links = {
   8: "https://example.com/path-to-m3u82.m3u8",
   9: "https://example.com/path-to-m3u81.m3u8",
   10:"https://vkvsd14.okcdn.ru/cmaf/7517976988391/sig/55r6yQdd0yw/expires/1736011005660/srcIp/13.127.152.182/urls/185.226.53.65/clientType/13/srcAg/CHROME/mid/9200744410087/get/hls_9200744410087.GYAkhiUddgo.m3u8",
-  11:"https://vkvsd55.okcdn.ru/cmaf/7465941011175/sig/uvJeXZhUciA/srcIp/13.201.182.224/urls/45.136.21.63/expires/1735856029357/clientType/13/srcAg/CHROME/fromCache/1/mid/9161388141543/id/7465941011175/get/hls_9161388141543.yi5Gc1pbmz0.m3u8",
+  11:"https://vkvsd63.okcdn.ru/cmaf/7465941011175/sig/pJfcnALbZ-s/expires/1736012567077/srcIp/13.201.97.130/urls/185.226.53.94/clientType/13/srcAg/CHROME/mid/9161388141543/get/hls_9161388141543.yi5Gc1pbmz0.m3u8",
   12:"https://m118753.protect-cdn.net/live/m1-index.m3u8",
 };
