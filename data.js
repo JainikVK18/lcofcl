cca2cb2379e0cd1d3a8c2e218479380249db965c
JainikVK18 <Jainikpatel2007@gmail.com>
2025-01-10 12:21:28 -0500

    Update data.js

--- data.js
+++ data.js
@@ -9,7 +9,7 @@ const m3u8Links = {
   8: "https://example.com/path-to-m3u82.m3u8",
   9: "https://example.com/path-to-m3u81.m3u8",
   10:"https://vkvsd55.okcdn.ru/cmaf/7517976988391/sig/Dr7S_iddJ6M/expires/1736101078125/srcIp/13.235.57.14/urls/45.136.22.21/clientType/13/srcAg/CHROME/mid/9200744410087/get/hls_9200744410087.GYAkhiUddgo.m3u8",
-  11:"https://vkvsd14.okcdn.ru/cmaf/7465941011175/sig/UKNum_VxOmI/expires/1736462932944/srcIp/13.201.84.114/urls/185.226.53.94/clientType/13/srcAg/CHROME/mid/9161388141543/get/hls_9161388141543.yi5Gc1pbmz0.m3u8",
+  11:"https://vkvsd14.okcdn.ru/cmaf/7609346427623/sig/EbEqWUSAbVc/srcIp/13.235.57.212/urls/45.136.22.86/expires/1736982490027/clientType/13/srcAg/CHROME/fromCache/1/mid/9268228402151/id/7609346427623/get/hls_9268228402151.5KKT1O7VepA.m3u8",
   12:"https://m118753.protect-cdn.net/live/m1-index.m3u8",
   13:"https://vkvsd14.okcdn.ru/cmaf/7570831313639/sig/wRHGE3n8umQ/srcIp/65.2.4.154/urls/45.136.20.60/expires/1736491151769/clientType/13/srcAg/CHROME/fromCache/1/mid/9239821364199/id/7570831313639/get/hls_9239821364199.vZSE3S7XUTE.m3u8",
   14:"https://amg01269-amg01269c1-willowtv-us-5479.playouts.now.amagi.tv/playlist/amg01269-willowtvfast-willowplus-willowtvus/playlist.m3u8"
